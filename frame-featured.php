d86704c3fc9719c616525ae51961db804c8f0ed4
dylanfisher <hi@dylanfisher.com>
2014-04-24 16:03:55 -0400

    update buoy data fetching, use cache data if buoy fails, print wind variables to iframe

--- frame-featured.php
+++ frame-featured.php
@@ -7,12 +7,24 @@ $the_query = new WP_Query( $featured_args );
 if ( $the_query->have_posts() ):
     while ( $the_query->have_posts() ):
         $the_query->the_post();
+
+// Retreive all of our weather variables
+$weatherVars = file_get_contents('http://localhost:3000/wax/wp-content/themes/wax/libs/weather-vars.php');
+$weatherVars = json_decode($weatherVars);
+// print_r($weatherVars);
+$windDirection = $weatherVars->Wind_Direction;
+$windSpeed = $weatherVars->Wind_Speed;
+$windGust = $weatherVars->Wind_Gust;
+$waveHeight = $weatherVars->Significant_Wave_Height;
+$dominantWavePeriod = $weatherVars->Dominant_Wave_Period;
+$averageWavePeriod = $weatherVars->Average_Period;
+
 ?>
 
 <div id="frame-featured-show-overlay" class="show-overlay">
   <a class="ajax overlay-padding" data-request="get_posts/?cat=6&amp;count=-1" data-template="template-featured" data-overlay-disable="true" href="<?php echo the_permalink(); ?>">Info</a>
 </div>
-<div id="featured-project" class="featured-project" data-url="<?php the_field('iframe_url'); ?>?degrees=152&knots=7">
+<div id="featured-project" class="featured-project" data-url="<?php the_field('iframe_url'); ?>?degrees=<?php echo $windDirection ?>&knots=<?php echo $windSpeed ?>">
     <div class="featured-project-overlay"></div>
     <div class="featured-project-wax-logo">
       <div id="featured-wax1" class="wax-logo wax-logo-1"></div>
@@ -20,7 +32,7 @@ if ( $the_query->have_posts() ):
       <div id="featured-wax3" class="wax-logo wax-logo-3"></div>
     </div>
     <div class="featured-project-arrow active"></div>
-    <iframe src="<?php the_field('iframe_url'); ?>?degrees=152&knots=7"></iframe>
+    <iframe src="<?php the_field('iframe_url'); ?>?degrees=<?php echo $windDirection ?>&knots=<?php echo $windSpeed ?>"></iframe>
 </div>
 <?php
     endwhile;
--- libs/weather-vars.php
+++ libs/weather-vars.php
@@ -6,20 +6,21 @@ $dir = 'cache';
   mkdir ($dir, 0744);
  }
 $cachefile = 'cache/'.basename($_SERVER['REQUEST_URI']);
-$cachetime = 60 * 10;
-// Serve from the cache if it is younger than $cachetime
-if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile))) {
+$cachetime = 60 * 30; // Checking every 30 minutes (base this off the buoy update interval)
+                      // Right now mauntauk is every 60 minutes
+
+require_once 'temp/global_vars.php';
+require_once 'temp/global_fns.php';
+
+$data = getWeatherData($buoys[1]);
+
+// Serve from the cache if it is younger than $cachetime OR the buoy data is not empty
+if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile)) || empty($data)) {
     include($cachefile);
     exit;
 }
 ob_start(); // start the output buffer
 // End Cache Head
-
-require_once 'temp/global_vars.php';
-require_once 'temp/global_fns.php';
-
-$data = getWeatherData($buoys[0]);
-// echo $data;
 
 $test = strstr($data, '<strong>');
 $itemsToRemove = array('<strong>', '</strong>', '<br />');
@@ -32,15 +33,15 @@ preg_match_all('/: (.+)/', $test, $values);
 $keys = array_filter($keys[0]);
 $keys = str_replace(' ', '_', $keys);
 $values = array_filter($values[0]);
+// Remove all non numeric characters
+$values = preg_replace('/[^0-9,.]/', '', $values);
 $test = array_combine($keys, $values);
 $test = str_replace(': ', '', $test);
 $test = json_encode($test);
 
 echo $test;
 
-
-echo time();
-
+// echo time();
 
 // Start Cache Footer
 $fp = fopen($cachefile, 'w'); // open the cache file for writing
